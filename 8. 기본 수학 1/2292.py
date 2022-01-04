297a25a064f5d7f579cd8119c671af54f8ba7d36
soobin <tnqls991211@naver.com>
2022-01-04 23:43:09 +0900

    feat: solve 2292

--- 8. 기본 수학 1/2292.py
+++ 8. 기본 수학 1/2292.py
@@ -1,14 +1,14 @@
-#2292 벌집
+# 2292 벌집
 
 n = int(input())
 
-room = 1 # 벌집 개수
-plus = 6
-count = 1
+room = 1  # 벌집 개수
+plus = 6  # 벌집 단계가 6의 배수로 증가
+count = 1  # 벌집 번호
 
 while n > room:
-  room += plus 
-  plus += 6 
-  count += 1  
+    room += plus
+    plus += 6
+    count += 1
 
 print(count)
